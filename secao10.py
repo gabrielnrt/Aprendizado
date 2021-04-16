2b48d008aac036bc25602f5b77cd24d14682e2b7
gabrielnrt <gabrielpv98@gmail.com>
2021-04-16 14:25:35 -0300

    A descrição dos erros está comentada no código

--- secao10.py
+++ secao10.py
@@ -4,4 +4,17 @@
 # - Umidade: 0 a 100
 # - Jogar: sim/nao
 
+###################################################################
+## Coisas que estão ruins:
+
+# Na coluna 'Aparencia':
+# - Há um elemento 'menos', fora do domínio 
+
+# Na coluna 'Temperatura':
+# - Há apenas um elemento fora do domínio, que é 1220 ºF
+
+# Na coluna 'Umidade':
+# - Há um elemento com NaN
+# - Há um elemento fora do domínio, que é 200
+
 from pandas import read_csv
