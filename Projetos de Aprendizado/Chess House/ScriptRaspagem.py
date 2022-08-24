deaa2ea78066f85288e2d31f796010609114a28e
gabrielnrt <gabrielpv98@gmail.com>
2022-08-24 13:30:45 -0300

    Otimização do algoritmo.

--- Projetos de Aprendizado/Chess House/ScriptRaspagem.py
+++ Projetos de Aprendizado/Chess House/ScriptRaspagem.py
@@ -18,22 +18,26 @@ tag = sopa.find(name = 'div', class_='item_sel_wrapper')
 #----------------------------------------------------------------------------------------
 
 ListaCodigos = []
-
-for subtag in tag.find_all(name = 'div', class_ = 'item_sel_outer_mid_desc_code'):
-    ListaCodigos.append(subtag.string)
-
 ListaNomes = []
-
-for subtag in tag.find_all(name = 'div', class_ = "item_sel_outer_mid_desc_title"):
-    ListaNomes.append(subtag.find(name='a').string)
-
 ListaPrecos = []
 
-for subtag in tag.find_all(name='span', class_='sale_price'):
-    ListaPrecos.append(subtag.string)
+
+for subtag in tag.find_all(name = 'div', class_ = 'item_sel_outer'):
+
+    codigo = subtag.find(name = 'div', class_ = 'item_sel_outer_mid_desc_code').string
+    ListaCodigos.append(codigo)
+
+    nome = subtag.find(name = 'div', class_ = "item_sel_outer_mid_desc_title").find(name = 'a').string
+    ListaNomes.append(nome)
+
+    preco = subtag.find(name='span', class_='sale_price').string
+    ListaPrecos.append(preco)
+
 
 #----------------------------------------------------------------------------------------
 
 dicionario = {'Código':ListaCodigos, 'Nome':ListaNomes, 'Preço':ListaPrecos}
 
 df = DataFrame(dicionario)
+
+print(df.head())
